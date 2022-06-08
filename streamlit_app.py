80c3b7763a33117711cc6cce974a7e8279dd12c2
Jone44 <107043488+Jone44@users.noreply.github.com>
2022-06-08 11:09:45 +0300

    Added recommentations for smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-streamlit.title('My parents new healthy dinner')
+streamlit.title('My parents new sad dinner')
 
 streamlit.header('Breakfast menu')
 
@@ -16,6 +16,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect('pick some fruits:', list(my_fruit_list.index))
+streamlit.multiselect('pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 streamlit.dataframe(my_fruit_list)

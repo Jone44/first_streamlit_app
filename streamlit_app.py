cec54187e6987076cb9b11e0f6b06382a59b7ff4
Jone44 <107043488+Jone44@users.noreply.github.com>
2022-06-12 20:52:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My moms new sad dinner')
 
@@ -21,16 +22,29 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect('pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+
+
+
+
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+
 streamlit.dataframe(fruits_selected)
-
-
 streamlit.header('Fruityvice Fruit advice')
-fruit_choice = streamlit.text_input('mitä hetelmää sä haluut?', 'kiwi')
-streamlit.write('The user entered', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('mitä hetelmää sä haluut?')
+  if not fruit choice:
+    streamlit.error("Please select a fruit")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+
                                    
                                   
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -40,6 +54,6 @@ my_data_row = my_cur.fetchall()
 streamlit.header("Hetelmälista sisältää:")
 streamlit.dataframe(my_data_row)
 
-fruit_choice = streamlit.text_input('What fruit to add?', 'kiwi')
-streamlit.write('Adding', fruit_choice)
+fruit_choice2 = streamlit.text_input('What fruit to add?', 'kiwi')
+streamlit.write('Adding', fruit_choice2)
 
